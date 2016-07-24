34f7b7c97566b34894cdd1bb37abe47d94ed8844
HillviewAdmin <admin@hillviewgroupinc.com>
2016-07-24 10:28:45 -0400

    Publish

--- WinnersOffice/Branch.sql
+++ WinnersOffice/Branch.sql
@@ -2,5 +2,14 @@
 (
 	[BranchID] INT NOT NULL PRIMARY KEY, 
     [BranchName] NVARCHAR(50) NOT NULL, 
-    [Address] NVARCHAR(60) NULL
+    [Address1] NVARCHAR(60) NOT NULL, 
+    [Address2] NVARCHAR(60) NULL, 
+    [City] NVARCHAR(30) NOT NULL, 
+    [StateProvince] NVARCHAR(80) NOT NULL, 
+    [PostalCode] NVARCHAR(15) NOT NULL, 
+    [Country] NVARCHAR(50) NOT NULL, 
+    [CreatedBy] INT NOT NULL, 
+    [CreateDate] DATETIME NOT NULL DEFAULT getdate(), 
+    [UpdatedBy] INT NOT NULL, 
+    [UpdateDate] DATETIME NOT NULL DEFAULT getdate()
 )
